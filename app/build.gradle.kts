301673049f6372347066432921402f8ffc8e2d2b
Nate Towsley <natetowsley@gmail.com>
2025-10-29 17:22:11 -0700

    Video 3 complete. Database complete.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,8 @@ dependencies {
 
     implementation(libs.room.runtime)
     annotationProcessor( libs.room.compiler)
+    val room_version = "2.8.3"
+
+    implementation("androidx.room:room-runtime:$room_version")
+//    annotationProcessor "androidx.room:room-compiler:$room_version"
 }
--- app/src/main/java/com/example/hw04_gymlog_v300/MainActivity.java
+++ app/src/main/java/com/example/hw04_gymlog_v300/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
 
-    private static final String TAG = "DAC GYMLOG";
+    public static final String TAG = "DAC GYMLOG";
     String mExercise = "";
     double mWeight = 0.0;
     int mReps = 0;
