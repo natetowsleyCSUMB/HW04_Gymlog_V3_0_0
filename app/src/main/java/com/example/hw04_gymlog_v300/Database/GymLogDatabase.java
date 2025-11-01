e8c97177ae26c910166c4b49a4b58642d37af223
Nate Towsley <natetowsley@gmail.com>
2025-10-31 18:35:43 -0700

    Video 7 progress.

--- app/src/main/java/com/example/hw04_gymlog_v300/Database/GymLogDatabase.java
+++ app/src/main/java/com/example/hw04_gymlog_v300/Database/GymLogDatabase.java
@@ -11,6 +11,7 @@ import androidx.room.TypeConverters;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.example.hw04_gymlog_v300.Database.entities.GymLog;
+import com.example.hw04_gymlog_v300.Database.entities.User;
 import com.example.hw04_gymlog_v300.Database.typeConverters.LocalDateTypeConverter;
 import com.example.hw04_gymlog_v300.MainActivity;
 
@@ -18,9 +19,10 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @TypeConverters(LocalDateTypeConverter.class)
-@Database(entities = {GymLog.class}, version = 1, exportSchema = false)
+@Database(entities = {GymLog.class, User.class}, version = 1, exportSchema = false)
 public abstract class GymLogDatabase extends RoomDatabase {
 
+    public static final String USER_TABLE = "user_table";
     private static final String DATABASE_NAME = "GymLog_database";
     public static final String GYM_LOG_TABLE = "gymLogTable";
 
@@ -54,9 +56,20 @@ public abstract class GymLogDatabase extends RoomDatabase {
         public void onCreate(@NonNull SupportSQLiteDatabase db) {
             super.onCreate(db);
             Log.i(MainActivity.TAG, "DATABASE CREATED!");
-            //TODO: add databaseWriteExecutor.execute(() -> {...}
+            databaseWriteExecutor.execute(() -> {
+                UserDAO dao = INSTANCE.userDAO();
+                dao.deleteAll();
+                User admin = new User("admin1", "admin1");
+                admin.setAdmin(true);
+                dao.insert(admin);
+
+                User testUser1 = new User("testuser1", "testuser1");
+                dao.insert(testUser1);
+            });
         }
     };
 
     public abstract GymLogDAO gymLogDAO();
+
+    public abstract UserDAO userDAO();
 }
